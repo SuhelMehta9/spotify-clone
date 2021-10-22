e411aeeedc45eaaebca79d3256c85fce234135db
pranshu786729 <70695663+pranshu786729@users.noreply.github.com>
2021-10-22 07:50:43 +0530

    Create reducer.js

---
+++ reducer.js
@@ -0,0 +1,22 @@
+export const intitialState ={
+ user: null,
+ playlists: false,
+ item: null
+};
+
+
+const reducer = (state,action) => {
+console.log(action);
+
+switch(action.type){
+  case 'SET_USER':
+      return {
+          ...state,
+          user : action.user,
+      };
+      default:
+          return state;
+ }
+}
+
+export default reducer;
